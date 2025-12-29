6e7c8b4d7c983f1c6d3834de088a3684e9f734b4
Saleem U <saleem.umar@thoughtfocus.com>
2025-12-29 19:04:23 +0000

    Resolved Version Catalog Conflict

--- android-app/build-logic/settings.gradle.kts
+++ android-app/build-logic/settings.gradle.kts
@@ -36,4 +36,9 @@ dependencyResolutionManagement {
     mavenCentral()
     gradlePluginPortal()
   }
+  versionCatalogs {
+    create("projectLibs") {
+      from(files("../gradle/libs.versions.toml"))
+    }
+  }
 }
